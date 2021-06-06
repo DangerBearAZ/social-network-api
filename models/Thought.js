09ad1a453fc37bef4472f234b4976017ed129471
claire ogesen <claire.ogesen.co@gmail.com>
2021-06-06 12:49:05 -0700

    thought

--- models/Thought.js
+++ models/Thought.js
@@ -1,23 +1,33 @@
-const { Schema, model, Types } = require('mongoose');
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateformat');
 
 const ThoughtSchema = new Schema({
     thoughtText: {
+        type: String,
         required: true,
         min: 1,
         max: 280
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now, 
+        get: createdAtVal => dateFormat(createdAtVal)
     },
     userName: {
         type: String,
         required: true
+    }
+},
+{
+    toJSON: {
+        vituals: true,
+        getters: true
     },
-    reactions: [reactionSchema]
-});
+    id: false
+}
+);
 
-const Comment = model('Comment', CommentSchema);
+const Thought = model ('Thought', ThoughtSchema);
 
-module.exports = Comment;  
+module.exports = Thought;  
 
